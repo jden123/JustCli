64e4a8e452c706a62f2538c4ab17a48535742dce
den <dennvk@gmail.com>
2016-01-11 21:00:14 +0400

    Test command with required argument was added.

---
+++ src/examples/JustCli.TestApp/Commands/ShowFileCreationDateCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using JustCli.Attributes;
+
+namespace JustCli.TestApp.Commands
+{
+    [Command("fcd", "Prints file creation date time.")]
+    class ShowFileCreationDateCommand : ICommand
+    {
+        // NOTE: it's required.
+        [CommandArgument("f", "file", Description = "File path.")]
+        public string FilePath { get; set; }
+
+        public bool Execute()
+        {
+            if (!File.Exists(FilePath))
+            {
+                Console.WriteLine("The file[{0}] does not exist. Please check the file path.", FilePath);
+                return false;
+            }
+
+            Console.WriteLine("Utc file creation time is {0}", File.GetCreationTimeUtc(FilePath));
+            return true;
+        }
+    }
+}
